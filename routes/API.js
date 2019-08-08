eab4713dc0f6f63fcf4edb94fd1d06c3e7623b90
nhu.huynh <nhu.huynh@vexere.com>
2019-08-08 17:16:44 +0700

    8/9/2019

--- routes/API.js
+++ routes/API.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var APIChamp = require('../models/APIChamp')
 const axios = require('axios');
-
+var fs=require('fs-extra')
 router.get('/', (req, res) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -26,6 +26,38 @@ router.get('/:infoChamp',(req,res)=>{
     .then(e=>res.json(Object.values(e.data.data)[0]))
     .catch(err=>res.status(400).send("Not Found"))
     
+})
+
+router.put('/:editchamp',(req,res)=>{
+    // APIChamp.findOne({name:req.body.name,_id:{$ne:req.body.id}},(err,champ)=>{
+    //     if(err) return console.log(err)
+    //     if(champ){
+    //         res.send("da ton ton tai")
+    //     }else{
+    //         APIChamp.findById(req.body.id,(err,champ)=>{
+    //             champ.name="";
+    //             champ.title="";
+    //             champ.blurb="";
+    //             champ.image.full=""
+    //             champ.save(err=>{
+    //                 if(err) return console.log(err)
+    //                 const path=`public/image/champion/${req.body.pimage}`
+    //                 fs.remove(path,err=>{
+    //                     if(err) console.log(err)
+    //                 })
+    //                 req.files.file.mv(`public/image/champion/${req.files.file.name}`)
+    //             })
+    //         })
+    //     }
+    // })
+    console.log(req.body)
+    console.log(req.files)
+    req.files.file.mv(`public/image/${req.files.file.name}`,err=>{
+        if(err) return console.log(err)
+    })
+    fs.remove('public/image/Aatrox.png',err=>{
+        if(err) console.log(err) 
+    })
 })
 module.exports = router
 
--- routes/upload.js
+++ routes/upload.js
@@ -1,16 +1,24 @@
 var express = require('express');
  var router = express.Router()
- router.post('/',(req,res)=>{
-     if(req.files===null){
-         return res.status(400).json({msg: 'No file uploaded'})
-     }
-     const file = req.files.file
-     file.mv(`public/image/${file.name}`,err=>{
-         if(err){
-             console.log(err);
-             return res.status(500).send(err);
-         }
-         res.json({fileName:file.name, filePath:`/uploads/${file.name}`});
-     })
- })
+//  router.post('/',(req,res)=>{
+//      if(req.files===null){
+//          return res.status(400).json({msg: 'No file uploaded'})
+//      }
+//      const file = req.files.file
+//      file.mv(`public/image/${file.name}`,err=>{
+//          if(err){
+//              console.log(err);
+//              return res.status(500).send(err);
+//          }
+//          res.json({fileName:file.name, filePath:`/uploads/${file.name}`});
+//      })
+//  })
+    router.post('/vd',(req,res)=>{
+        if(req.files===null){
+                      return res.status(400).json({msg: 'No file uploaded'})
+                 }
+        console.log(req.files)
+        console.log(req.body.ho)
+        console.log(req.body.ten)
+    })
  module.exports = router
