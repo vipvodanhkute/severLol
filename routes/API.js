36d0d9f25f0ac40e75fdd07ad69314f42e7a1a1b
nhu.huynh <nhu.huynh@vexere.com>
2019-08-14 00:18:43 +0700

    put

--- routes/API.js
+++ routes/API.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var APIChamp = require('../models/APIChamp')
 const axios = require('axios');
-var fs=require('fs-extra')
+var fs = require('fs-extra')
 router.get('/', (req, res) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,45 +18,41 @@ router.get('/', (req, res) => {
 //         res.json(champ)
 //     })
 // })
-router.get('/:infoChamp',(req,res)=>{
+router.get('/:infoChamp', (req, res) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    const champ=async()=>axios.get(`http://ddragon.leagueoflegends.com/cdn/6.24.1/data/vn_VN/champion/${req.params.infoChamp}.json`)
+    const champ = async () => axios.get(`http://ddragon.leagueoflegends.com/cdn/6.24.1/data/vn_VN/champion/${req.params.infoChamp}.json`)
     champ()
-    .then(e=>res.json(Object.values(e.data.data)[0]))
-    .catch(err=>res.status(400).send("Not Found"))
-    
+        .then(e => res.json(Object.values(e.data.data)[0]))
+        .catch(err => res.status(400).send("Not Found"))
+
 })
 
-router.put('/:editchamp',(req,res)=>{
-    // APIChamp.findOne({name:req.body.name,_id:{$ne:req.body.id}},(err,champ)=>{
-    //     if(err) return console.log(err)
-    //     if(champ){
-    //         res.send("da ton ton tai")
-    //     }else{
-    //         APIChamp.findById(req.body.id,(err,champ)=>{
-    //             champ.name="";
-    //             champ.title="";
-    //             champ.blurb="";
-    //             champ.image.full=""
-    //             champ.save(err=>{
-    //                 if(err) return console.log(err)
-    //                 const path=`public/image/champion/${req.body.pimage}`
-    //                 fs.remove(path,err=>{
-    //                     if(err) console.log(err)
-    //                 })
-    //                 req.files.file.mv(`public/image/champion/${req.files.file.name}`)
-    //             })
-    //         })
-    //     }
-    // })
-    console.log(req.body)
-    console.log(req.files)
-    req.files.file.mv(`public/image/${req.files.file.name}`,err=>{
-        if(err) return console.log(err)
-    })
-    fs.remove('public/image/Aatrox.png',err=>{
-        if(err) console.log(err) 
+router.put('/', (req, res) => {
+    APIChamp.findOne({ name: req.body.name, key: { $ne: req.body.key } }, (err, champ) => {
+        if (err) return console.log(err)
+        if (champ) {
+            res.json({ type: "danger", text: "Please choose name another" })
+        } else {
+            res.json({ type: "success", text: "Eidted" })
+            APIChamp.find({ key: req.body.key }, (err, champ) => {
+                // console.log(champ)
+                
+                // champ.name = req.body.name
+                // champ.title = req.body.title;
+                // champ.blurb = req.body.blurb;
+                // // if(req.files){
+                // //     champ.image.full=req.files.image.name   
+                // // }
+                champ.save()
+                 /*const path=`public/image/champion/${req.body.pimage}`
+                 fs.remove(path,err=>{
+                    if(err) console.log(err)
+                 })
+                 req.files.image.mv(`public/image/champion/${req.files.image.name}`)*/
+                
+            })
+        }
     })
 })
 module.exports = router
